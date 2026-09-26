0a72d297ed221adb115c5b09965454e40195ac58
Russ White <356303+russwyte@users.noreply.github.com>
2026-09-25 21:40:42 -0500

    L5: legacy-only change

--- modules/legacy/src/main/scala/lab/legacy/Checksum.scala
+++ modules/legacy/src/main/scala/lab/legacy/Checksum.scala
@@ -1,5 +1,5 @@
 package lab.legacy
 
 object Checksum {
-  def of(text: String): Int = text.foldLeft(0)((acc, c) => (acc * 31 + c) & 0x7fffffff)
+  def of(text: String): Int = text.foldLeft(0)((hash, ch) => (hash * 31 + ch) & 0x7fffffff)
 }
